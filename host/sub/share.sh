f9d888ae21ea01291cb34b45ca771b8c2f34bace
aguy tech <aguytech@free.fr>
2022-06-23 19:33:58 +0200

    host: add host v0.1

---
+++ host/sub/share.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo usermod -G libvirt-qemu -a ${USER}
+
+[ -d "${_PATH_SHARE}" ] || sudo mkdir -p "${_PATH_SHARE}"
+sudo chown -R libvirt-qemu:libvirt-qemu ${_PATH_SHARE}
+sudo setfacl -Rm g:libvirt-qemu:rwx ${_PATH_SHARE}
+sudo setfacl -d -Rm g:libvirt-qemu:rwx ${_PATH_SHARE}
+
+_echoyb "Configure in virt-manager the shared path like this with this paramaters:"
+echo "Source path: ${_PATH_SHARE}"
+echo "Target path: ${_PATH_SHARE}"
+_echoyb "\nAnd add fmode and dmode by modifying XML settings options in virt-manager:"
+echo -e '<filesystem type="mount" accessmode="mapped" '${yellow}'fmode="0660" dmode="0770"'${cclear}'>
+  <source dir="/vms/share"/>
+  <target dir="/hostshare"/>
+  <address type="pci" domain="0x0000" bus="0x07" slot="0x00" function="0x0"/>
+</filesystem>'
+_ask
