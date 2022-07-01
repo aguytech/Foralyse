5505e999e4f1798047f4e17f9da191790649b3b8
aguy tech <aguytech@free.fr>
2022-07-01 11:48:30 +0200

    guest: mount-cases.sh updates _PARTUUID option + add error ouptut

--- guest/xtra/mount-cases.sh
+++ guest/xtra/mount-cases.sh
@@ -13,11 +13,19 @@ __mount() {
 	eval "$( echo $1 | cut -d' ' -f2- | tr ' ' '\n' | sed 's|^|_|' )"
 	#echo -e "_DEV=${_DEV} \n_LABEL=${_LABEL} \n_UUID=${_UUID} \n_BLOCK_SIZE=${_BLOCK_SIZE} \n_TYPE=${_TYPE} \n_PARTUUID=${_PARTUUID}"
 	path=${_PATH_CASE}/${_LABEL}_${_UUID}
+	[ -z "${_UUID}" ] && path=${_PATH_CASE}/${_LABEL}_${_PARTUUID}
+
 	[ -d "${path}" ] || mkdir "${path}"
+
 	if grep -q "${path}" /proc/mounts; then
 		echo "${path} already mounted"
 	else
-		mount UUID=${_UUID} ${path}
+		if [ "${_UUID}" ]; then
+			mount UUID=${_UUID} ${path}
+		else
+			mount PARTUUID=${_PARTUUID} ${path}
+		fi
+		echo "${path} mounted"
 	fi
 }
 
@@ -26,11 +34,15 @@ __umount() {
 	eval "$( echo $1 | cut -d' ' -f2- | tr ' ' '\n' | sed 's|^|_|' )"
 	#echo -e "_DEV=${_DEV} \n_LABEL=${_LABEL} \n_UUID=${_UUID} \n_BLOCK_SIZE=${_BLOCK_SIZE} \n_TYPE=${_TYPE} \n_PARTUUID=${_PARTUUID}"
 	path=${_PATH_CASE}/${_LABEL}_${_UUID}
+	[ -z "${_UUID}" ] && path=${_PATH_CASE}/${_LABEL}_${_PARTUUID}
+
 	if grep -q "${path}" /proc/mounts; then
 		umount ${path}
+		echo "${path} unmounted"
 	else
-		echo "${path} not mounted"
+		echo "${path} already unmounted"
 	fi
+
 	[ -d "${path}" ] && rmdir "${path}"
 }
 
@@ -51,9 +63,9 @@ __init() {
 	script=${0##*/}
 
 	# case path
-	! [ -d "${_PATH_CASE}" ] && echo "Unable to find path: ${_PATH_CASE}" && exit 1
+	! [ -d "${_PATH_CASE}" ] && echo "Unable to find path: _PATH_CASE=${_PATH_CASE}" && exit 1
 	# Wrong parameters numbers
-	[ "$#" -lt 1 ] && echo "Wrong parameters numbers: $#" && __usage
+	[ "$#" -lt 1 ] && echo "Wrong parameters numbers: $#" >&2 && __usage
 }
 
 _PATH_CASE=
@@ -63,5 +75,5 @@ __init $*
 case $1 in
 	mount)	__cases $1 ;;
 	umount)	__cases $1 ;;
-	*)				echo "Wrong command given to ${script}"; __usage ;;
+	*)				echo "Wrong command given to ${script}" >&2; __usage ;;
 esac
