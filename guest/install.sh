4c368943c8e364cd5162ee1ebdd83322625e4b56
aguy tech <aguytech@free.fr>
2022-07-14 19:14:17 +0200

    guest: add _HALT & update with _CONFSET

--- guest/install.sh
+++ guest/install.sh
@@ -19,7 +19,7 @@ file=${_PATH_BASE}/../inc
 ! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
 ! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
 
-######################## BEGIN
+######################## DATA
 
 _echoA "- Use from the GUEST"
 
@@ -27,22 +27,30 @@ if [ -z "${_PATH_SHARE}" ]; then
 	anstmp=/foralyse/share
 	_askno "Give the shared path from the Guest (${anstmp})"
 	_PATH_SHARE=${_ANSWER:-${anstmp}}
-	sed -i "/^_PATH_SHARE=/ s|=.*$|=${_PATH_SHARE}|" ${_FILE_CONF}
+	_confset _PATH_SHARE "${_PATH_SHARE}"
 fi
 
 if [ -z "${_PATH_CASE}" ]; then
 	anstmp=/foralyse/cases
 	_askno "Give the path to mount cases (${anstmp})"
 	_PATH_CASE=${_ANSWER:-${anstmp}}
-	sed -i "/^_PATH_CASE=/ s|=.*$|=${_PATH_CASE}|" ${_FILE_CONF}
+	_confset _PATH_CASE "${_PATH_CASE}"
 fi
 
 if [ -z "${_PATH_NBD}" ]; then
 	anstmp=/foralyse/nbd
 	_askno "Give the path to mount dumped disk (${anstmp})"
 	_PATH_NBD=${_ANSWER:-${anstmp}}
-	sed -i "/^_PATH_NBD=/ s|=.*$|=${_PATH_NBD}|" ${_FILE_CONF}
+	_confset _PATH_NBD "${_PATH_NBD}"
 fi
+
+if [ -z ${_HALT+x} ]; then
+	_askyn "Enable halt between each parts?"
+	_HALT=${_ANSWER/n/}
+	_confset _HALT "${_HALT}"
+fi
+
+######################## BEGIN
 
 # share
 if grep -q "^/hostshare.*${_PATH_SHARE}" /etc/fstab \
